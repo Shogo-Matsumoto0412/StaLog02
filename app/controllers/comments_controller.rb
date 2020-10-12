57b32f9356d0f688e7e55a0bd99ca909f1450ee4
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-13 01:01:57 +0900

    commentsコントローラー　destroyアクション　設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,12 @@ class CommentsController < ApplicationController
         redirect_back(fallback_location: root_path) 
     end
 
+    def destroy
+        @comment = Comment.find_by(id: params[:id])
+        @comment.destroy
+        redirect_back(fallback_location: root_path) 
+    end
+
     private
     def comment_params
         params.require(:comment).permit(:content, :post_id).merge(user_id: current_user.id)
