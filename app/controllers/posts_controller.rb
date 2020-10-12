39a3da152fe7356b89d4aa66b02d759527fba18a
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-12 15:08:44 +0900

    postsコントローラー　作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:post_content, :field_content, :count_hour).merge(user_id: current_user.id)
   end
 end
