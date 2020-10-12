1b6a16f2c80373d6a3b5b2f6d2056c0a04275a96
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-12 14:48:02 +0900

    fieldsコントローラー　設定

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,4 +1,26 @@
 class FieldsController < ApplicationController
   def index
+    @fields = Field.all
+  end
+
+  def new
+    @field = Field.new
+  end
+
+  def create
+    @field = Field.new(field_params)
+    @field.save
+    redirect_to fields_path
+  end
+
+  def destroy
+    @field = Field.find_by(id: params[:id])
+    @field.destroy
+    redirect_to fields_path
+  end
+
+  private
+  def field_params
+    params.require(:field).permit(:title).merge(user_id: current_user.id)
   end
 end
