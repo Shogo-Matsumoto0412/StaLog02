07a9f338426f159d0d11193656089569ace93818
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-12 15:08:26 +0900

    postsルーティング　設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :fields, only: [:index, :new, :create, :show, :destroy]
   resources :profile, only: [:show]
   devise_for :users
+  resources :posts
   root 'posts#index'
 end
