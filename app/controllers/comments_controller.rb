6856937f2dfc0d8b234b4589b7e623195639f7a7
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-13 00:38:33 +0900

    commentsコントローラー　修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.save
+        redirect_back(fallback_location: root_path) 
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content, :post_id).merge(user_id: current_user.id)
+    end
 end
