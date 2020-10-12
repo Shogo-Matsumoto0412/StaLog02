889d5294859d7531a41f4b065500176350f83a90
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-12 16:24:49 +0900

    relationshipモデル　作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_064401) do
+ActiveRecord::Schema.define(version: 2020_10_12_071949) do
 
   create_table "fields", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 2020_10_12_064401) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
