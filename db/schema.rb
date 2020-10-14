1ea0a28052c721f8e0f2bd8d70c749b858fc0d10
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-15 02:07:42 +0900

    roomモデル　作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_170646) do
+ActiveRecord::Schema.define(version: 2020_10_14_170722) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -66,6 +66,11 @@ ActiveRecord::Schema.define(version: 2020_10_14_170646) do
     t.index ["follow_id"], name: "index_relationships_on_follow_id"
     t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
     t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
