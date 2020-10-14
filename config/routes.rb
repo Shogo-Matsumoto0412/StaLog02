c1c1e04379b4ffbc09ed954f062d082d7e16fe15
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-15 02:11:47 +0900

    DM機能　ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :fields, only: [:index, :new, :create, :show, :destroy]
   resources :profile, only: [:show]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
   devise_for :users
   resources :posts do
     resources :comments, only: [:create, :destroy]
