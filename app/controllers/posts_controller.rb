8d1593bf4b3cb5d97b13b20eb8da5d2988b44263
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-16 16:29:23 +0900

    postsコントローラー　createアクション　修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.save
-    redirect_to root_path 
+    flash[:notice] = "投稿しました"
+    redirect_to root_path
   end
 
   def destroy
