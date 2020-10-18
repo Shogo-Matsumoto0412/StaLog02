15528641d79274ecf089f6c6defe7d2f24519430
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-19 00:58:20 +0900

    usersテーブル　goalカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_081118) do
+ActiveRecord::Schema.define(version: 2020_10_18_155753) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_081118) do
     t.string "name"
     t.string "profile_image"
     t.text "profile_text"
+    t.text "goal"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
