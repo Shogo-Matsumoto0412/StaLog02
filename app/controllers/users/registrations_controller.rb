6c544a1e720a903eea02e12b3ecfaef50238476f
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-19 01:04:06 +0900

    goalビュー追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     protected
   
     def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_text, :profile_image])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_text, :profile_image, :goal])
     end
 
     def update_resource(resource, params)
@@ -12,10 +12,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
 
     def after_update_path_for(_resource)
-        root_path
+        profile_path(current_user.id)
     end
 
     def configure_account_update_params
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_text, :profile_image])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_text, :profile_image, :goal])
     end
 end
