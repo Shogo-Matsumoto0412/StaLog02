6ebda3a48d6a32ea54d4c99f0bbf86f2d1541d06
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2020-10-23 19:06:31 +0900

    日本語化

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module MyApp71
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :ja
+
     config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
